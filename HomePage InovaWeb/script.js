38de92280b87b08b79175d67d161e1d42fa6d4ec
JOAO MARCELLO SANTOS DIAS OLIVEIRA <joao.marcello@aluno.senac.rio>
2025-09-12 19:08:22 -0300

    add: modal no projeto

--- HomePage InovaWeb/script.js
+++ HomePage InovaWeb/script.js
@@ -1,5 +1,55 @@
 
+
+
+
+const buyModal = document.getElementById('buyModal')
+
 function openModal() {
-   const buyModal = document.getElementsByClassName('.buy-modal').
+   buyModal.style.display = 'flex'
+   document.body.style.overflow = 'hidden'
 
 }
+
+function closeModal() {
+   buyModal.style.display = 'none'
+   document.body.style.overflow = ''
+}
+
+
+const form = document.getElementById("formulario");
+
+form.addEventListener("submit", function(e) {
+   e.preventDefault(); 
+
+   const name = document.getElementById("name");
+   const email = document.getElementById("email");
+   const password = document.getElementById("password");
+
+   if (name.value.trim() === "") {
+      alert("Nome não pode estar vazio");
+      name.style.border = '1px solid red'
+      return;
+   }
+
+   if (email.value.trim() === "") {
+      alert("Email não pode estar vazio");
+      email.style.border = '1px solid red'
+      return;
+   }
+
+   if (password.value.trim() === "") {
+      alert("Senha não pode estar vazia");
+      password.style.border = '1px solid red'
+      return;
+   }
+
+   alert("Formulário enviado com sucesso!");
+   form.submit();
+});
+
+closeModal()
+
+
+
+
+    
