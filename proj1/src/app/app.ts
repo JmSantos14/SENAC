41a18e9a07870845557ce218cbc5c1b968ae8845
JOAO MARCELLO SANTOS DIAS OLIVEIRA <joao.marcello@aluno.senac.rio>
2025-09-24 18:38:14 -0300

    add: angular

--- proj1/src/app/app.ts
+++ proj1/src/app/app.ts
@@ -9,4 +9,11 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('proj1');
+  titulo:string = "Ja sei como usar";
+  imagem:string = "https://cdn.pixabay.com/photo/2020/02/10/18/22/eagle-4837272_1280.jpg"
+
+  pessoa = {
+    nome: "João",
+    idade: 21
+  }
 }
